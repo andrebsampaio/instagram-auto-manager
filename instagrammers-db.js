cf1f64639c233b26c767d6579243fa84a83f3701
andrebsampaio <andrebsampaio@gmail.com>
2017-10-14 13:23:52 +0100

    Upload from instagrammers incomplete

---
+++ instagrammers-db.js
@@ -0,0 +1,21 @@
+exports.instagrammers = {
+	'photography' : [
+	{'username':'brahmino'},
+	{'username':'witchoria'},
+	{'username':'Macenzo'},
+	{'username':'janske'},
+	{'username':'asasjostromphotography'},
+	{'username':'daveyoder'},
+	{'username':'thiswildidea'},
+	{'username':'mikekus'},
+	{'username':'danrubin'},
+	{'username':'darrylljones'},
+	{'username':'memoryweaver'},
+	{'username':'mkshunter'},
+	{'username':'janesamuels'},
+	{'username':'dvl'},
+	{'username':'astrodub'},
+	{'username':'chiaraverdo'},
+]
+};
+
