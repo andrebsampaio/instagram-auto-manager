557b426add3570706b3c569225ce978c2a5cdff5
andrebsampaio <andrebsampaio@gmail.com>
2017-10-15 22:27:56 +0100

    Fixed caption problems + Multiple fixes

--- auto-manager.js
+++ auto-manager.js
@@ -1,4 +1,65 @@
 var InstagramAPI = require('./instagram-api');
-var Client = require('instagram-private-api').V1;
+var fs = require('fs');
+var async = require('async');
+var request = require('request');
+var imgExtension = '.jpg';
+var imgFolder = './tmp/';
+var dbPath = 'instagrammers-db';
+var instaLocalDB = JSON.parse(fs.readFileSync(dbPath, 'utf8'))[0];
 
-new InstagramAPI('zixam1805', 'password123456789', __dirname + '\cookies\zixam1805').uploadImage('novalincsbg.jpg', 'fuck yeah');
+var api = new InstagramAPI('zixam1805', 'password123456789', __dirname + '/cookies/');
+
+
+var download = function(uri, filename, callback) {
+    request.head(uri, function(err, res, body) {
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback(filename));
+    });
+};
+
+var deleteFile = function(filename) {
+    fs.unlink(filename, function(err) {
+        if (err && err.code == 'ENOENT') {
+            // file doens't exist
+            console.info("File doesn't exist, won't remove it.");
+        } else if (err) {
+            // other errors, e.g. maybe we don't have enough permission
+            console.error("Error occurred while trying to remove file");
+        }
+    });
+}
+
+var getValidURI = function(urls) {
+    if (urls === undefined || !urls.length) {
+        throw new Error('URLs is empty');
+    }
+    for (url of urls) {
+        if (url !== undefined) {
+            return url;
+        }
+    }
+}
+
+var uploadImageFromUser = function(username, caption, callback) {
+    console.log("Now uploading " + username);
+    return api.getImagesFromUser(username)
+        .then(function(urls) {
+            if (!fs.existsSync(imgFolder)) {
+                fs.mkdirSync(imgFolder);
+            }
+            download(getValidURI(urls), imgFolder + username + imgExtension, function(filename) {
+                return function() {
+                    api.uploadImage(filename, caption, callback);
+                }
+            });
+        });
+}
+
+async.eachSeries(instaLocalDB.photography, function iteratee(item, callback) {
+    uploadImageFromUser(item.username, "Photo by: @" + item.username, function(imagePath, result) {
+        deleteFile(imagePath);
+        callback(null);
+    }).catch(function(e) {
+        console.log(e);
+        callback(null);
+    });
+});
---
+++ instagram-api.js
@@ -0,0 +1,59 @@
+var Client = require('instagram-private-api').V1;
+var _ = require('lodash');
+var Promise = require('bluebird');
+var fs = require('fs');
+
+module.exports = class InstagramAPI {
+
+    constructor(clientName, password, storagePath) {
+        this.device = new Client.Device(clientName);
+        if (!fs.existsSync(storagePath)) {
+            fs.mkdirSync(storagePath);
+        }
+        this.storage = new Client.CookieFileStorage(storagePath + clientName);
+        this.clientName = clientName;
+        this.password = password;
+    }
+
+    getSession() {
+        return Client.Session.create(this.device, this.storage, this.clientName, this.password);
+    }
+
+    uploadImage(imagePath, imageCaption, callback) {
+        this.getSession().then(function(session) {
+            Client.Upload.photo(session, imagePath)
+                .then(function(upload) {
+                    Client.Media.configurePhoto(session, upload.params.uploadId, imageCaption)
+                        .then(function(result) {
+                            callback(imagePath, result);
+                        });
+                });
+        });
+    }
+
+    findAccount(username) {
+        return this.getSession().then(function(session) {
+            return Client.Account.searchForUser(session, username)
+        });
+    }
+
+    getImagesFromUser(username) {
+        var api = this;
+        return this.getSession()
+            .then(function(session) {
+                return [session, api.findAccount(username)];
+            })
+            .spread(function(session, account) {
+                var feed = new Client.Feed.UserMedia(session, account.id);
+
+                return feed.get().then(function(results) {
+                    // result should be Media[][]
+                    var media = _.flatten(results);
+                    var urls = _.map(media, function(medium) {
+                        return _.first(medium.params.images).url;
+                    });
+                    return urls;
+                });
+            });
+    }
+};
