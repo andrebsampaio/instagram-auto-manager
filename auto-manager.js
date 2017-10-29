bd08f1b93dd60326981b7ee375018358ba31b7ae
andrebsampaio <andrebsampaio@gmail.com>
2017-10-29 17:59:35 +0000

    Changed hashtag list

--- auto-manager.js
+++ auto-manager.js
@@ -49,7 +49,7 @@ var uploadImageFromUser = function(username, caption, oldAs, callback) {
         });
 }
 
-var hashtags = "#photography #love #instagood #follow #followme #followback #instafollow #photo #photos #pic #pics #picture #pictures #snapshot #art #beautiful #follow4follow #instagood #picoftheday #photooftheday #color #all_shots #exposure #composition #focus #capture #moment"
+var hashtags = "#agameoftones #ig_masterpiece #ig_exquisite #ig_shotz #global_hotshotz #superhubs #main_vision #master_shots #exclusive_shots #hubs_united #worldshotz #theworldshotz #pixel_ig #photographyislifee #photographyislife #photographysouls #photographyeveryday #photographylover #worldbestgram #iglobal_photographers #ig_great_pics #ig_myshot #shotwithlove #justgoshoot #xposuremag #icatching #collectivelycreate #wanderlust #heatercentral #highsnobiety"
 
 var runInstagrammersDBUpload = function (topic){
     async.eachSeries(instaLocalDB[topic], function iteratee(item, callback) {
